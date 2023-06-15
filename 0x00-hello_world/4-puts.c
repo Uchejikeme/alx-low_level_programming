088b36c21e4035064133515e9f2d5a2e4cb536ea
Uchejikeme <uche2737@gmail.com>
2023-06-15 12:55:31 -0700

    Hello World Task Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,7 @@
  * Return: Alway 0 (Success)
  */
 int main(void)
-(
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
-
- )
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
