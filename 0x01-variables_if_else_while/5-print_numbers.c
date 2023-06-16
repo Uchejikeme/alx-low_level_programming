0a81790003136ec5ef8b340e908cc6a479a45c3e
Uchejikeme <uche2737@gmail.com>
2023-06-16 13:35:59 -0700

    Numbers, Task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point below
+ *        Prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: All the time 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
