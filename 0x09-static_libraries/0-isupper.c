c8c1d86abafad838ee708e72de7d4e69221b9c67
Uchejikeme <uche2737@gmail.com>
2023-07-07 09:00:57 -0700

    Uche 0x09. C - Static libraries Task

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - please check uppercase alphabets
+ * @c: character
+ * Return: 1 if c is the  uppercase otherwise 0.
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
